0edc85ec1199e2d8751c7ad17ad2d98e680a42f8
Daniel James <businessjames1999@gmail.com>
2020-02-21 08:39:27 -0500

    Added campground part of the set up

--- 09_Capstone/Capstone.Tests/Setup.sql
+++ 09_Capstone/Capstone.Tests/Setup.sql
@@ -4,5 +4,7 @@ delete from park
 delete from campground
 
 insert into park
-(name, location, establish_date, area, visitors,description)
-values('Twilight Park', 
+(name, location, establish_date, area, visitors, description)
+values('Twilight Park', 'Ohio', '1999-12-25', 9001, 2000000, 'A beautiful park that has a great sunset, perfect for eating ice cream!')
+
+Declare @newPark_Id int = (SELECT @@IDENTITY);
