59a5c05d94dea871cd0a66a3269f27a663a8d407
Daniel James <businessjames1999@gmail.com>
2020-02-21 08:31:06 -0500

    added a little bit to the set up

--- 09_Capstone/Capstone.Tests/Setup.sql
+++ 09_Capstone/Capstone.Tests/Setup.sql
@@ -1,1 +1,8 @@
-﻿
+﻿Delete From site
+delete from reservation
+delete from park
+delete from campground
+
+insert into park
+(name, location, establish_date, area, visitors,description)
+values('Twilight Park', 
